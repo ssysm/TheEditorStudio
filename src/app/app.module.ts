55385ab2c689a43b69b702d07464d4ee71ee466b
theeditor <syua9708@flhs.us>
2018-01-12 13:20:09 -0500

    form and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
+
+import {RootController} from "./controller/root";
+import { NotFoundComponent } from './controller/not-found/not-found.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RootController
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/controller/root.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {NotFoundComponent} from "./not-found/not-found.component";
+
+const routes: Routes = [
+  {
+    path:'404',
+    component:NotFoundComponent
+  },
+  {
+    path:'**',
+    redirectTo:'/404'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class RootController { }
